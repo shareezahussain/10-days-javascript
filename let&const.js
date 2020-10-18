f2a22e8b11cea54c76b1fb8ce7fea7c091d5c55c
Shareeza Hussain <46433886+shareezahussain@users.noreply.github.com>
2020-10-18 17:46:56 -0400

    Create let&const.js

---
+++ let&const.js
@@ -0,0 +1,12 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+
+    const PI = Math.PI;
+    let r = readLine();
+    
+    // Print the area of the circle:
+    let area = PI * (r*r);
+    let perimeter = 2 * PI * r;
+       console.log(area);
+    // Print the perimeter of the circle:
+    console.log(perimeter);
